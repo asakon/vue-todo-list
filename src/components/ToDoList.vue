776b20ad37dc3624f5d1a3917e581f8827bc7453
Asako Kondo <kondoasako@gmail.com>
2021-07-03 10:14:46 +0900

    双方向データバインディング

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,4 +1,6 @@
 <template>
+<input v-model="inputValue">
+入力した値：{{ inputValue }}
 <ul>
   <li v-for="todo in todoItems"
     v-bind:key="todo.id">
@@ -9,6 +11,7 @@
 export default {
   data() {
     return {
+      inputValue: '',
       todoItems: [
         { id: 1, text: '海に行って、砂遊びを友達とする' },
         { id: 2, text: '図書館に行って、マンガを借りる' }
